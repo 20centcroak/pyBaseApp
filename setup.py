5a77f49ea12d756f7f900ee829a62c759cc2d067
20centcroak <vpaveau@outook.com>
2020-10-04 19:09:33 +0200

    release 0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pyBaseApp',         # How you named your package folder (MyLib)
   packages = ['applauncher','package'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Tools to define a standard way to launch and package python application',   # Give a short description about your library
   author = '20centCroak',                   # Type in your name
   author_email = 'vpaveau@outlook.com',      # Type in your E-Mail
   url = 'https://github.com/20centcroak/pyBaseApp',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/20centcroak/pyBaseApp/archive/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/20centcroak/pyBaseApp/archive/0.2.tar.gz',    # I explain this later on
   keywords = ['LAUNCHER', 'PACKAGER', 'LAUNCH', 'PACKAGE', 'STANDARD', 'LOGGER', 'BASIC', 'APP'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pyyaml',
